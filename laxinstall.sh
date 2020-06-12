92c81f7094eead93715c8eb3f9ff79869a21b03f
devlintrace <41807964+devlintrace@users.noreply.github.com>
2020-06-12 15:58:32 -0500

    Update laxinstall.sh

--- laxinstall.sh
+++ laxinstall.sh
@@ -36,6 +36,7 @@ sudo usermod -aG docker $USERNAME >>/dev/null 2>&1
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose >>/dev/null 2>&1
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+sudo dnf install git -y
 }
 apt=`command -v apt-get`
 yum=`command -v yum`
